5c88f60becadf2a3f94db7ccffe81e261a5cbe29
ahoang2502 <athoang2502@gmail.com>
2023-08-30 00:09:36 +1000

    added faq-page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import FormPage from "./scenes/form";
 import Calendar from "./scenes/calendar";
+import FAQPage from "./scenes/faq";
 
 import { Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -30,10 +31,10 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<FormPage />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQPage />} />
               {/*  <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,5 +1,107 @@
-import { Box, useTheme } from "@mui/material";
-import Accordion from "@mui/material";
-import AccordionSummary from "@mui/material";
+import {
+  Box,
+  useTheme,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import { ExpandMore } from "@mui/icons-material";
 
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+import React from "react";
+
+const FAQPage = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+
+      <Accordion defaultExpanded sx={{ backgroundColor: colors.primary[400] }}>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            quae magnam possimus similique nesciunt aperiam cupiditate in labore
+            nemo dignissimos.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded sx={{ backgroundColor: colors.primary[400] }}>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Favorite Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            quae magnam possimus similique nesciunt aperiam cupiditate in labore
+            nemo dignissimos.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded sx={{ backgroundColor: colors.primary[400] }}>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            quae magnam possimus similique nesciunt aperiam cupiditate in labore
+            nemo dignissimos.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded sx={{ backgroundColor: colors.primary[400] }}>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            quae magnam possimus similique nesciunt aperiam cupiditate in labore
+            nemo dignissimos.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded sx={{ backgroundColor: colors.primary[400] }}>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            A Random Question
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            quae magnam possimus similique nesciunt aperiam cupiditate in labore
+            nemo dignissimos.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
+
+export default FAQPage;
